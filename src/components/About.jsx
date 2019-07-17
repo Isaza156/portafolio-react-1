c16db5e0f2fe8f712f47f46f00781154c0940932
Sala 310 <brahianestiven08@gmail.com>
2019-07-17 12:31:12 -0500

    act

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/* const About = function(){
+    return <h1>Hola Mundo Geek!</h1>
+}
+
+const AboutDos = () => {
+    return <h1>Hola Mundo Geek!</h1>
+} */
+
+// Retornar de manera explicita
+const About = () => (<h1>Hola Mundo Geek!</h1>);
+
+export default About;
